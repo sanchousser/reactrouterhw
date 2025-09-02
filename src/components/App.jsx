dd4c0fc0f5a880a4c3d21942b1ef01ad8f778e0f
sanchousser <golyukaleks@gmail.com>
2025-09-02 19:09:05 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,18 @@
 import { HomePage } from "pages/HomePage";
+import { MoviesPage } from "pages/MoviesPage";
 import { Route, Routes } from "react-router-dom";
+import { Navigation } from "./Navigation";
 
 export const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<HomePage />}/>
+        <Route path='/' element={<Navigation/>}>
+          <Route path="/" element={<HomePage />} />
+          <Route path='/movies' element={<MoviesPage />} />
+        </Route>
+
       </Routes>
-      <Outlet></Outlet>
 
     </div>
   );
